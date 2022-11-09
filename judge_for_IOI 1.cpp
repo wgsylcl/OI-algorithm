80ea85b3276a053af828eaa176402a4eea050e07
wgsylcl <1216862532@qq.com>
2022-11-09 15:09:23 +0800

    new file:   .vscode/c_cpp_properties.json 	modified:   judge_for_IOI 1.cpp 	modified:   judge_for_IOI.cpp 	new file:   judge_for_IOI.exe 	modified:   tempCodeRunnerFile.cpp

--- judge_for_IOI 1.cpp
+++ judge_for_IOI 1.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 #include<cstdio>
-#include"oi.h"
-#include"answer.h"
 using namespace std;
 
 int main()
--- judge_for_IOI.cpp
+++ judge_for_IOI.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<cstdio>
-#include"oi.h"
-#include"answer.h"
+
 using namespace std;
 
 int main()
